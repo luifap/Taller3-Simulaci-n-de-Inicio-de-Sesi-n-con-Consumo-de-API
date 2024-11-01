69497476208d6a0b08c6a964e166e6424a217eac
luifap <luifa07@gmail.com>
2024-10-31 21:00:51 -0500

    Cambios de pagina inicio

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} /> 
+        <Route path="/" element={<Navigate to="/register" />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
